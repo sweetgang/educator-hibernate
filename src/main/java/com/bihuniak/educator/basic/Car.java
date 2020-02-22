6e4c4791d673cc52ac1248607bef5f02e87de83b
sweetgang <lider218@gmail.com>
2020-02-22 15:33:57 +0100

    version 1.2

--- src/main/java/com/bihuniak/educator/basic/Car.java
+++ src/main/java/com/bihuniak/educator/basic/Car.java
@@ -1,9 +1,6 @@
 package com.bihuniak.educator.basic;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Car {
@@ -13,10 +10,62 @@ public class Car {
     private long id;
 
 
-
+    @Access(value = AccessType.PROPERTY)
+    @Basic(optional = false)
     private String model;
+
     private String brand;
     private int wheels;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model + "F16";
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public double getEngineCapasity() {
+        return engineCapasity;
+    }
+
+    public void setEngineCapasity(double engineCapasity) {
+        this.engineCapasity = engineCapasity;
+    }
+
+    public boolean isUseed() {
+        return useed;
+    }
+
+    public void setUseed(boolean useed) {
+        this.useed = useed;
+    }
+
+    @Column(name = "Capasity", nullable = false)
     private double engineCapasity;
     private boolean useed;
 
